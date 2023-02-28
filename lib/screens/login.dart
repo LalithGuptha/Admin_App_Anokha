e880ec0665aaa22838ce090ee6d88e6d4ecd29b7
Lalith Guptha <lalithg95@gmail.com>
2023-02-28 12:31:20 +0530

    Login Screen

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:hexcolor/hexcolor.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -22,7 +22,79 @@ class _Login extends State<Login> {
           child:Column(
             children: [
               Container(
-                child: Text("Welcome"),
+                margin: EdgeInsets.all(40),
+                child: Column(
+                  children: [
+                    Padding(padding: EdgeInsets.only(top: 40),
+                      child: Align(
+                      alignment: Alignment.center,
+                      child: Image.asset('assets/logo.png'),
+                    ),),
+                    SizedBox(height: 80,),
+
+                    TextField(
+                      controller: uncontroller,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(16)),
+                        labelText: "Username",
+                        hintText: "Username"
+                      ),
+                    ),
+                    SizedBox(height: 40),
+                    TextField(
+                      controller: uncontroller,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(16)),
+                          labelText: "Password",
+                          hintText: "Password"
+                      ),
+                    ),
+                    SizedBox(height: 32,),
+                    SizedBox(
+                      height: 50,
+                      width: 160,
+                      child: ElevatedButton(
+                        onPressed: (){},
+                        child: Text("Login",style: TextStyle(
+                            fontSize: 16
+                        ),),
+                        style: ElevatedButton.styleFrom(
+                          primary: HexColor("FF7F11"),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(16))
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                        padding: EdgeInsets.only(top: 56),
+                    child: Text("Not a registered user?",style: TextStyle(
+                        fontSize: 18
+                    ),)
+                    ),
+                    SizedBox(height: 24,),
+                    SizedBox(
+                      height: 50,
+                      width: 160,
+                      child: ElevatedButton(
+                        onPressed: (){},
+                        child: Text("Register",style: TextStyle(
+                            fontSize: 16
+                        ),),
+                        style: ElevatedButton.styleFrom(
+                          primary: HexColor("FF7F11"),
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(16))
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+
+                ),
               )
             ],
           )
